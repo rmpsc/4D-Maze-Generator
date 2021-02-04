bbde7007ccd5b69635923a54274dc827a8bb448c
Romel Pascua <romelpascua14@gmail.com>
2021-02-04 13:56:43 -0800

    clean up code
    removed unwanted functions/variables

--- src/Cell.java
+++ src/Cell.java
@@ -1,10 +1,7 @@
 public class Cell {
 
-    char name;
     int walls;
-    int[] location;
     Cell parent;
-    int index;
     int t;
     int z;
     int y;
@@ -16,19 +13,50 @@ public class Cell {
     }
 
     // CONSTRUCTOR
-    Cell(char name, int walls, int[] location, Cell parent, int index) {
-        this.name = name;
+    Cell(int walls, Cell parent, int t, int z, int y, int x) {
         this.walls = walls;
-        this.location = location;
         this.parent = parent;
-        this.index = index;
+        this.t = t;
+        this.z = z;
+        this.y = y;
+        this.x = x;
     }
 
     // GETTERS
     public int getWalls() { return walls; }
 
+    public Cell getParent() { return parent; }
+
+    public int getT() { return t; }
+    public int getZ() { return z; }
+    public int getY() { return y; }
+    public int getX() { return x; }
+
+    // SETTERS
+    public void setWalls(int walls) { this.walls = walls; }
+
+    public void setParent(Cell parent) { this.parent = parent; }
+
+    public void setT(int t) { this.t = t; }
+    public void setZ(int z) { this.z = z; }
+    public void setY(int y) { this.y = y; }
+    public void setX(int x) { this.x = x; }
+
+    // MISC
+    public void changeWall(int change) {
+        walls = walls & ~(1 << (change - 1));
+    }
+
     public int getWall(int dir) {
         return (walls >> (dir - 1)) & 1;
+    }
+
+    public int getExactCoord(int coord) {
+        if (coord == 0) { return t; }
+        if (coord == 1) { return z; }
+        if (coord == 2) { return y; }
+        if (coord == 3) { return x; }
+        return 0;
     }
 
     public String getLocation() {
@@ -52,46 +80,5 @@ public class Cell {
             default:
                 return "default switch case";
         }
-    }
-
-    public int getExactCoord(int coord) {
-        if (coord == 0) { return t; }
-        if (coord == 1) { return z; }
-        if (coord == 2) { return y; }
-        if (coord == 3) { return x; }
-        return 0;
-    }
-
-    public Cell getParent() { return parent; }
-
-    public char getName() { return name; }
-
-    public int getT() { return t; }
-    public int getZ() { return z; }
-    public int getY() { return y; }
-    public int getX() { return x; }
-    public void setT(int t) { this.t = t; }
-    public void setZ(int z) { this.z = z; }
-    public void setY(int y) { this.y = y; }
-    public void setX(int x) { this.x = x; }
-    // SETTERS
-    public void setWalls(int walls) { this.walls = walls; }
-
-    public void setLocation(int[] location) { this.location = location; }
-
-    public void setParent(Cell parent) { this.parent = parent; }
-
-    public void setName(char name) { this.name = name; }
-
-    public void setIndex(int index) { this.index = index; }
-
-    public void changeWall(int change) {
-        walls = walls & ~(1 << (change - 1));
-    }
-    
-    @Override
-    public String toString() {
-        return Integer.toString(walls);
-        
     }
 }
