2efc43b9e413499729b69b64d84f9a83d6352f75
Romel Pascua <romelpascua14@gmail.com>
2021-02-03 17:57:05 -0800

    implement maze algorithm

--- src/Cell.java
+++ src/Cell.java
@@ -4,7 +4,7 @@ public class Cell {
     int walls;
     int[] location;
     Cell parent;
-    Cell[] neighbors;
+    int index;
 
     // DEFAULT CONSTRUCTOR
     Cell() {
@@ -12,18 +12,42 @@ public class Cell {
     }
 
     // CONSTRUCTOR
-    Cell(char name, int walls, int[] location, Cell parent) {
+    Cell(char name, int walls, int[] location, Cell parent, int index) {
         this.name = name;
         this.walls = walls;
         this.location = location;
         this.parent = parent;
+        this.index = index;
     }
 
     // GETTERS
     public int getWalls() { return walls; }
 
+    public int getWall(int dir) {
+        return (walls >> (dir - 1)) & 1;
+    }
+
     public String getLocation() {
-        return "[" + location[0] + ", " + location[1] + ", " + location[2] + ", " + location[3] + "]";
+        return location[0] + "" + location[1] + "" + location[2] + "" + location[3];
+    }
+
+    public String getNeighbor(int direction, int coordinate) {
+        switch (coordinate) {
+            case 0:
+                if (direction % 2 == 0) { return (location[0] - 1) + "" + location[1] + "" + location[2] + "" + location[3]; }
+                else { return (location[0] + 1) + "" + location[1] + "" + location[2] + "" + location[3]; }
+            case 1:
+                if (direction % 2 == 0) { return location[0] + "" + (location[1] - 1) + "" + location[2] + "" + location[3]; }
+                else { return location[0] + "" + (location[1] + 1) + "" + location[2] + "" + location[3]; }
+            case 2:
+                if (direction % 2 == 0) { return location[0] + "" + location[1] + "" + (location[2] - 1) + "" + location[3]; }
+                else { return location[0] + "" + location[1] + "" + (location[2] + 1) + "" + location[3]; }
+            case 3:
+                if (direction % 2 == 0) { return location[0] + "" + location[1] + "" + location[2] + "" + (location[3] - 1); }
+                else { return location[0] + "" + location[1] + "" + location[2] + "" + (location[3] + 1); }
+            default:
+                return "default switch case";
+        }
     }
 
     public Cell getParent() { return parent; }
@@ -39,18 +63,17 @@ public class Cell {
 
     public void setName(char name) { this.name = name; }
 
+    public void setIndex(int index) { this.index = index; }
+
     public void changeWall(int change) {
         // holds bit value to be checked
         int bit = (walls >> (change - 1)) & 1;
-        System.out.println("bit before = " + bit);
-        if (bit == 0) {
-            walls = walls | (1 << (change - 1));
-        }
-        else {
-            walls = walls & ~(1 << (change - 1));
-        }
+        // System.out.println("bit before = " + bit);
+        
+        walls = walls & ~(1 << (change - 1));
+        
         bit = (walls >> (change - 1)) & 1;
-        System.out.println("bit after = " + bit);
+        // System.out.println("bit after = " + bit);
     }
     
 
--- src/StudentSolver.java
+++ src/StudentSolver.java
@@ -4,17 +4,19 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class StudentSolver {
     public static void main(String[] args) {
-        byte[] alienMaze = solve(3);
+        byte[] alienMaze = solve(2);
         
         for (int i = 0; i < alienMaze.length; i++) {
-            // System.out.println(alienMaze[i]);
+            System.out.println("index[" + i + "] = " + Byte.toUnsignedInt(alienMaze[i]));
         }
         System.out.println("done");
 
     }
 
     public static byte[] solve(int n) {
+        
         int mazeSize = (int) Math.pow(n, 4);
+        System.out.println("mazeSize = " + mazeSize);
         // array to fit all walls
         byte[] maze = new byte[mazeSize];
         // array to fit all cell objects
@@ -34,25 +36,30 @@ public class StudentSolver {
         );
 
         int index = 0;
+        int nIndex = 0;
+        int randDir = 0;
+        int coordinate = 0;
+        int wallState = 0;
+        int wallsHit = 0;
 
         // makes all cells with proper locations
         for (int t = 0; t < n; t++) {
             for (int z = 0; z < n; z++) {
                 for (int y = 0; y < n; y++) {
                     for (int x = 0; x < n; x++) {         
-                        cells[index] = new Cell('z', 0b11110111, new int[]{t, z, y, x}, null);
+                        cells[index] = new Cell('z', 0b11111111, new int[]{t, z, y, x}, null, 0);
                         // points to itself afters its created
                         cells[index].setParent(cells[index]);
-                        
                         // gives each cell a name
                         cells[index].setName((char) ('A' + index));
-
+                        // assigns index
+                        cells[index].setIndex(index);
                         // allows cell to be reached by location
-                        // location : index
                         cellMap.put(t + "" + z + "" + y + "" + x, index);
                         // System.out.println(cells[index].name);
                         // System.out.println(t + "" + z + "" + y + "" + x);
                         // System.out.println("index = " + index);
+                        System.out.println(cells[index].getWalls());
                         index += 1;
                     }
                 }
@@ -60,41 +67,56 @@ public class StudentSolver {
         }
 
         // starting cell
-        Cell current = cells[40];
+        Cell current = cells[0];
+        cells[0].parent = cells[1];
 
-        // obtains wall inside boundaries
-        // odd = pos dir    even = neg dir
-        int randDir = ThreadLocalRandom.current().nextInt(1, 9);
-        int coordinate = (int) coordMap.get(randDir);
-        while ((current.location[coordinate] == 0 && randDir % 2 == 0) || (current.location[coordinate] == n - 1 && randDir % 2 == 1)) {
+        while (wallsHit < mazeSize - 1) {
+            // obtains wall inside boundaries
+            // odd = pos dir    even = neg dir
             randDir = ThreadLocalRandom.current().nextInt(1, 9);
             coordinate = (int) coordMap.get(randDir);
-        }
-        System.out.println("randDir = " + randDir);
-        System.out.println("coordinate = " + coordinate);
-        current.changeWall(randDir);
-
-        /*
-        NEIGHBOR CODE
-        // get neighbors of starting cell making sure to skip boundaries
-        // for every var tzyx, if valid add to neighbors
-        for (int i = 0; i < 4; i++) {
-            if (current.location[i] == 0) {
-                if (i == 0) { int nIndex = (int) cellMap.get(current.location[i] + 1 + "" + current.location[1] + "" + current.location[2] + "" + current.location[3]); }
-
+            wallState = current.getWall(randDir);
+            while ((current.location[coordinate] == 0 && randDir % 2 == 0) || (current.location[coordinate] == n - 1 && randDir % 2 == 1)) {
+                randDir = ThreadLocalRandom.current().nextInt(1, 9);
+                coordinate = (int) coordMap.get(randDir);
+                wallState = current.getWall(randDir);
             }
-        }
-        */
 
+            // obtains next cell
+            nIndex = (int) cellMap.get(current.getNeighbor(randDir, coordinate));
+
+            // if wall is closed, check if you can break
+            // if wall is open, move to that cell
+            if (wallState == 1 && cells[nIndex].parent == cells[nIndex]) {
+                // break wall in cell
+                current.changeWall(randDir);
+
+                // must break neighbors wall as well
+                if (randDir % 2 == 0) { cells[nIndex].changeWall(randDir - 1); }
+                else { cells[nIndex].changeWall(randDir + 1); }
+
+                // change neighbors parent to current cell
+                cells[nIndex].parent = current;
+
+                // increment walls hit
+                wallsHit += 1;
+                System.out.println("wallsHit = " + wallsHit);
+            }
+
+            current = cells[nIndex];
+
+        }
+
+        for (int i = 0; i < mazeSize; i++) {
+            maze[i] = (byte) cells[i].walls;
+            System.out.println(cells[i].getName() + " at " + cells[i].getLocation() + " = " + cells[i].getWalls());
+        }
         // transfers walls to maze array
         System.out.println("transferring");
         for (int i = 0; i < mazeSize; i++) {
-            maze[i] = (byte) cells[i].walls;
+            maze[i] = (byte) cells[i].walls; 
         }
-
 
         return maze;
     }
-    
-
 }
